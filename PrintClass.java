020e39a6e120d53f499ca7c5e48161a23cfa724c
mike <mike@mikes-Air.attlocal.net>
2017-06-03 20:36:20 -0500

    Mike changed

--- PrintClass.java
+++ PrintClass.java
@@ -3,6 +3,9 @@ public class PrintClass {
 	public static void print(String take){
 		System.out.print(take);
 	}
+	/**
+	prints line
+	 */
 	public static void println(String take){
 		System.out.println(take);
 	}
